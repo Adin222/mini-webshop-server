64cee86137a70c673260177f825f08d0acb3a93c
Adin222 <Adin222>
2025-07-30 00:40:18 +0200

    feat: add route again :(

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 origins = [
     "http://localhost:5173", 
     "http://127.0.0.1:5173",
-    "https://papaya-squirrel-d752e0.netlify.app"
+    "https://clinquant-hotteok-2e9352.netlify.app"
 ]
 
 app.add_middleware(
